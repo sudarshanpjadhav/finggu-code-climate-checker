4885d8a6dc34437e2f9bf9f338c2220eacbe5429
Sudarshan P Jadhav <42689334+sudarshanpjadhav@users.noreply.github.com>
2026-07-07 11:35:22 +0530

    finggu-asf: add/update src/main/java/com/finggu/codeclimatechecker/FingguCodeClimateCheckerApplication.java

---
+++ src/main/java/com/finggu/codeclimatechecker/FingguCodeClimateCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.finggu.codeclimatechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingguCodeClimateCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FingguCodeClimateCheckerApplication.class, args);
+    }
+
+}
